e4d39852d80d190f4d0d7367ec414391f086f9ac
Balaji Ganesh <30574948+DarkyNinjaa009@users.noreply.github.com>
2018-12-06 13:15:54 +0530

    Update main.cpp

--- pong/SRC/main.cpp
+++ pong/SRC/main.cpp
@@ -12,37 +12,10 @@ using namespace game;
 
 int main()
 {	
-	//gamemanager g(40, 20);
-	//g.run();
-	//system("PAUSE");
-	GLFWwindow * window;
-
-	glfwInit();
-
-	window = glfwCreateWindow(640, 480, "Hello world", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		std::cout << "Failed to load windows";
-		system("PAUSE");
-		return 0;
-	}
-	glfwMakeContextCurrent(window);
-	while (!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glBegin(GL_QUADS);
-		glVertex2d(-0.5f, -0.5f);
-		glVertex2d(-0.5f, 0.5f);
-		glVertex2d(0.5f, 0.5f);
-		glVertex2d(0.5f, -0.5f);
-		glEnd();
-		glfwPollEvents();
-		glfwSwapBuffers(window);
-
-		
-	}
-	glfwPollEvents();
+	gamemanager g(40, 20);
+	g.run();
+	system("PAUSE");
+	
 	
 	return 0;
 
